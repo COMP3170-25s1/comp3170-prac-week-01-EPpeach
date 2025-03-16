a0a613b1b739be3001931c238ca89b9da7eeaeb2
EPpeach <elswyth.porter@students.mq.edu.au>
2025-03-17 00:11:02 +1100

    Edit the Shader: Draw a diamond
    I'm... not sure why that worked... to be honest

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -8,8 +8,8 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
    //float d = distance(p, vec2(0.25, 0.30));     // calculate distance to midpoint
-   vec2 v = abs(p - vec2(0.5, 0.5));
-   float d = min(v.x, v.y); 
+   vec2 v = abs(p - vec2(0.25, 0.30));
+   float d = v.x+v.y;                             //Is this bc diagonal distances look further?
   
    if (d < 0.12) {
       o_colour = vec4(u_colour, 1);
