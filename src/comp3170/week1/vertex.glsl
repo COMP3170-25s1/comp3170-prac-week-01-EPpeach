dba5681190f194192ba123a9d8116b0f84d9225f
EPpeach <elswyth.porter@students.mq.edu.au>
2025-03-18 15:18:01 +1100

    I think I edited Vertex by mistake? maybe?

--- src/comp3170/week1/vertex.glsl
+++ src/comp3170/week1/vertex.glsl
@@ -1,8 +1,13 @@
 #version 410
 
 in vec4 a_position;	/* vertex position */
+in vec3 a_colour;
+out vec3 v_colour;   // RGB to the fragment shader
 
 void main() {
     gl_Position = a_position;
+    
+    //Pass through here to fragement shader
+    v_colour = a_colour;
 }
 
